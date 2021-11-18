fdc314f9b93a6611c174eab924eee27f04a69cae
zeonplamo <zeonplamo@gmail.com>
2021-11-18 23:56:33 +0700

    Debug button
    Putting it here until the flexbox dynamic buttons work.

--- planesmith/app/src/main/java/umn/ac/id/planesmith/MainActivity.java
+++ planesmith/app/src/main/java/umn/ac/id/planesmith/MainActivity.java
@@ -1,14 +1,26 @@
 package umn.ac.id.planesmith;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button next = (Button) findViewById(R.id.debugbuttontemp);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), landing.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
     }
 }
---
+++ planesmith/app/src/main/java/umn/ac/id/planesmith/landing.java
@@ -0,0 +1,14 @@
+package umn.ac.id.planesmith;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class landing extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing);
+
+        }
+}
