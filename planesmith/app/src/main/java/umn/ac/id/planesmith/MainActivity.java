ee296ceda04ee8de7c1a75f3388b377871ecb5c0
zeonplamo <zeonplamo@gmail.com>
2021-11-20 16:22:56 +0700

    One Story

--- planesmith/app/src/main/java/umn/ac/id/planesmith/MainActivity.java
+++ planesmith/app/src/main/java/umn/ac/id/planesmith/MainActivity.java
@@ -8,19 +8,11 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button next = (Button) findViewById(R.id.debugbuttontemp);
-        next.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), landing.class);
-                startActivityForResult(myIntent, 0);
-            }
-        });
-    }
+    };
 }
